4a89b979b4521e5eae420eedddab20cb81c7663e
Giancarlo Valentino Risopatron Moraga <giancarlo.risopatron1601@alumnos.ubiobio.cl>
2021-03-10 14:55:55 -0300

    cambios en main, index.py

--- backend/connect.py
+++ backend/connect.py
@@ -1,37 +1,57 @@
 import psycopg2 
+import importlib
 
 #coneccion a la base de datos
 
-def connection() :
-
-    con = psycopg2.connect(
-
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="importante")
-
-    #cursor
-    #cur=con.cursor()
-
-    #ur.execute("select * from Usuario")
-
-#rows=cur.fetchall()
-
-#for r in rows:
- #   print(f"id {r[0]} correo electronico {r[1]} contraseña {r[2]}")
 
 
 
-# cerrar la base da datos
-    #con.close()
 
-    return con
+class BDD():
+
+    def __init__(self):
+        self.con=None
+        self.cur=None
+
+    def conectar(self):
+        self.con=psycopg2.connect(  
+                host="localhost",
+                database="postgres",
+                user="postgres",
+                password="importante")
+
+    def generarcursor(self):
+        self.cur=self.con.cursor
+        
+
+    def insertar(self,grupo,columnas,valores):
+        
+        concatenacion_01="".join(columnas)
+        concatenacion_02="".join(valores)
+        syntaxis="insert into "+grupo+" "+concatenacion_01+" values "+concatenacion_02
+
+        self.cur.execute(syntaxis)
+        self.con.commit()
+
+         
+    def seleccionarTodos(self,grupo):
+
+        syntaxis="select * from "+grupo
+
+        self.cur.execute(syntaxis)
+        rows=self.cur.fetchall()
+        return rows
+
+    def seleccionar(self,grupo,columnas):
+
+        syntaxis="select ("+columnas+") from "+grupo
+
+        self.cur.execute(syntaxis)
+        rows=self.cur.fetchall()
+        return rows
 
 
-def insertUser(self):
-    if  self.connection() :
-        print("conexion exitosa equisde")
 
-    else :
-        print("gg wp no funciono")
+    def desconectar(self):
+        self.con.close()
+
